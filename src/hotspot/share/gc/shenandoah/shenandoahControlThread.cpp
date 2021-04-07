f4b509070d4291baa60895f281f5a7fe2cd87ec8
William Kemper <kemperw@amazon.com>
2021-04-07 13:24:35 -0700

    Increase mutex rank of gc waiters
    This lets us take the control thread's lock to notify it without tripping the deadlock detector.

--- src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
@@ -51,7 +51,7 @@
 ShenandoahControlThread::ShenandoahControlThread() :
   ConcurrentGCThread(),
   _alloc_failure_waiters_lock(Mutex::leaf, "ShenandoahAllocFailureGC_lock", true, Monitor::_safepoint_check_always),
-  _gc_waiters_lock(Mutex::leaf, "ShenandoahRequestedGC_lock", true, Monitor::_safepoint_check_always),
+  _gc_waiters_lock(Mutex::safepoint, "ShenandoahRequestedGC_lock", true, Monitor::_safepoint_check_always),
   _control_lock(Mutex::leaf, "ShenandoahControlGC_lock", true, Monitor::_safepoint_check_never),
   _periodic_task(this),
   _requested_gc_cause(GCCause::_no_cause_specified),
