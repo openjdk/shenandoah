305a918e65b85c0465ae03d4011f76da5bef8757
Christian Young <cyyoungm@amazon.com>
2021-07-09 22:14:14 -0700

    Changed ShenandoahLogFileOutput to independent class and added copyrights

--- src/hotspot/share/gc/shenandoah/shenandoahLogFileOutput.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahLogFileOutput.cpp
@@ -1,6 +1,10 @@
 /*
+<<<<<<< HEAD
  * Copyright (c) 2021, Amazon.com, Inc. All rights reserved.
  * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+=======
+ * Copyright (c) 2013, 2021, Red Hat, Inc. All rights reserved.
+>>>>>>> 6be92fadc66 (Changed ShenandoahLogFileOutput to independent class and added copyrights)
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
