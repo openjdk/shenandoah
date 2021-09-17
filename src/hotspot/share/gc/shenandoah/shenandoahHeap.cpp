82cd9ba115a6b7ededbc81156b00389db718e35d
William Kemper <kemperw@amazon.com>
2021-09-17 14:20:02 -0700

    Only treat promotions as old gen allocations

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -1078,9 +1078,6 @@ HeapWord* ShenandoahHeap::allocate_memory(ShenandoahAllocRequest& req) {
         pacer()->unpace_for_alloc(pacer_epoch, requested - actual);
       }
     } else {
-      if (req.is_old()) {
-        old_generation()->increase_allocated(actual_bytes);
-      }
       increase_used(actual_bytes);
     }
   }
