917831c562fc7fca561ac5e4bb6e85bc8b01d112
Kelvin Nilsen <kdnilsen@amazon.com>
2022-05-12 14:37:02 +0000

    Add missing semicolon

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -2464,7 +2464,7 @@ private:
                 // Need to examine both dirty and clean cards during mixed evac.
                 oop obj = cast_to_oop(r->bottom());
                 size_t obj_size = obj->size();
-                size_t card_size_in_words = CardTable::card_size_in_words()
+                size_t card_size_in_words = CardTable::card_size_in_words();
                 size_t size_up = (obj_size - 1 + card_size_in_words) & ~(card_size_in_words - 1);
                 r->oop_iterate_humongous_slice(&cl, false, r->bottom(), size_up, true, CONCURRENT);
               } else {
