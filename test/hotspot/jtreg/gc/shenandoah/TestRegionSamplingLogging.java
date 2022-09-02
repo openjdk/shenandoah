22dfc26a3131192aaccc946effbfc3b6803c30be
Celine Wu <cfumiwu@amazon.com>
2022-09-02 23:11:33 +0000

    Delete the ShenandoahLogRegionSampling from the globals.hpp

--- test/hotspot/jtreg/gc/shenandoah/TestRegionSamplingLogging.java
+++ test/hotspot/jtreg/gc/shenandoah/TestRegionSamplingLogging.java
@@ -28,7 +28,7 @@
  *
  * @run main/othervm -Xmx1g -Xms1g -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+ShenandoahRegionSampling -XX:+ShenandoahRegionSampling
- *      -XX:+ShenandoahLogRegionSampling -Xlog:gc+region=debug:region-snapshots-%p.log::filesize=100,filecount=3
+ *      -Xlog:gc+region=debug:region-snapshots-%p.log::filesize=100,filecount=3
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=adaptive
  *      TestRegionSamplingLogging
  */
