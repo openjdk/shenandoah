0d599ce3dd78f6c68b66203ba86bdf580f881e75
Ramki Ramakrishna <ysr@amazon.com>
2023-05-20 02:47:59 +0000

    Actually add the ShenandoahAgeCensus class files to repo.

---
+++ src/hotspot/share/gc/shenandoah/shenandoahAgeCensus.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#include "gc/shenandoah/mode/shenandoahGenerationalMode.hpp"
+#include "gc/shenandoah/shenandoahAgeCensus.hpp"
+#include "gc/shenandoah/shenandoahHeap.hpp"
+
+ShenandoahAgeCensus::ShenandoahAgeCensus() {
+  assert(ShenandoahHeap::heap()->mode()->is_generational(), "Only in generational mode");
+  const int max_age = markWord::max_age;
+  _global_age_table = NEW_C_HEAP_ARRAY(AgeTable*, max_age, mtGC);
+  for (int i = 0; i < max_age; i++) {
+    _global_age_table[i] = new AgeTable(false);
+    // Note that we don't now get perfdata from age_table
+  }
+  if (!GenShenCensusAtEvac) {
+    size_t max_workers = ShenandoahHeap::heap()->max_workers();
+    _local_age_table = NEW_C_HEAP_ARRAY(AgeTable*, max_workers, mtGC);
+    for (uint i = 0; i < max_workers; i++) {
+      _local_age_table[i] = new AgeTable(false);
+    }
+    _epoch = max_age - 1;  // see update_epoch()
+  }
+}
+
+// Update the epoch for the global age tables,
+// and merge local age tables into the global age table.
+void ShenandoahAgeCensus::update_epoch() {
+  if (++_epoch == markWord::max_age) {
+    _epoch=0; 
+  } 
+  // Merge data from local age tables into the global age table for the epoch,
+  // clearing the local tables.
+  _global_age_table[_epoch]->clear();
+  size_t max_workers = ShenandoahHeap::heap()->max_workers();
+  for (uint i = 0; i < max_workers; i++) {
+    _global_age_table[_epoch]->merge(_local_age_table[i]);
+    _local_age_table[i]->clear();
+  }
+  _global_age_table[_epoch]->print_age_table(InitialTenuringThreshold);
+} 
+
+
+// Reset the epoch for the global age tables,
+// clearing all history.
+void ShenandoahAgeCensus::reset_epoch() {
+  for (uint i = 0; i < markWord::max_age; i++) {
+    _global_age_table[i]->clear();
+  }
+  _epoch = markWord::max_age - 1;
+}
---
+++ src/hotspot/share/gc/shenandoah/shenandoahAgeCensus.hpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef SHARE_GC_SHENANDOAH_SHENANDOAHAGECENSUS_HPP
+#define SHARE_GC_SHENANDOAH_SHENANDOAHAGECENSUS_HPP
+
+#include "gc/shared/ageTable.hpp"
+
+// A class for tracking a sequence of cohort population vectors (age tables)
+// for up to C age cohorts. We track up to C historical population vectors,
+// to track temporal variation of cohort demographics. Since there are at most
+// C age cohorts, we need only track at most C snapshots to track a maximal
+// pre-promotion demographics of any object in the young generation.
+// The _global_matrix is thus a C x C matrix, with C = 16, currently, see
+// MAX_COHORTS below.
+//
+// In addition, we maintain per worker vectors into which census for the current
+// minor GC is tracked during marking. These are cleared after each marking cycle,
+// once the per-worker data is consolidated into the appropriate population vector
+// at each minor collection. The _local_matrix is thus C x N, for N GC workers.
+class ShenandoahAgeCensus: public CHeapObj<mtGC> {
+  AgeTable** _global_age_table;      // Global age table used for adapting tenuring threshold
+  AgeTable** _local_age_table;       // Local scratch age tables to track object ages
+  uint _epoch;                       // Current epoch (modulo max age)
+
+ public:
+  enum {
+    MAX_COHORTS = AgeTable::table_size
+  };
+
+  ShenandoahAgeCensus();
+
+  AgeTable* get_local_age_table(uint worker_id) const { return (AgeTable*) _local_age_table[worker_id]; }
+  AgeTable* get_age_table()                     const { return (AgeTable*) _global_age_table[_epoch];   }
+  void update_epoch();
+  void reset_epoch();
+};
+
+#endif // SHARE_GC_SHENANDOAH_SHENANDOAHAGECENSUS_HPP
