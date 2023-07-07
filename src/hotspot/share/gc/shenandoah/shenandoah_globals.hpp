ac5878abcde450337485dc58a05e3077cf2de116
Ramki Ramakrishna <ysr@amazon.com>
2023-07-07 00:48:42 +0000

    Lower the threshold of cohort mortality for longevity. Add a legal range for option.

--- src/hotspot/share/gc/shenandoah/shenandoah_globals.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoah_globals.hpp
@@ -54,12 +54,13 @@
           range(0,16)                                                       \
                                                                             \
   product(double, ShenandoahGenerationalTenuringMortalityRateThreshold,     \
-                                                         0.3, EXPERIMENTAL, \
+                                                         0.1, EXPERIMENTAL, \
           "(Generational mode only) Cohort mortality rates below this "     \
           "value will be treated as indicative of longevity, leading to "   \
           "tenuring. A lower value delays tenuring, a higher value hastens "\
           "it. Used only when ShenandoahGenerationalhenAdaptiveTenuring is "\
           "enabled.")                                                       \
+          range(0.001,0.999)                                                \
                                                                             \
   product(size_t, ShenandoahGenerationalTenuringCohortPopulationThreshold,  \
                                                          4*K, EXPERIMENTAL, \
