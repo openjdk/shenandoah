556e5cd8c60cfc18547a4bb90399e2cefbb14e95
Ramki Ramakrishna <ysr@amazon.com>
2024-06-20 02:02:59 +0000

    Revert debugging detritus.

--- test/hotspot/jtreg/gc/shenandoah/oom/TestThreadFailure.java
+++ test/hotspot/jtreg/gc/shenandoah/oom/TestThreadFailure.java
@@ -78,7 +78,6 @@ public class TestThreadFailure {
         {
             ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(
                     "-Xmx32m",
-                    // "-XX:+UnlockExperimentalVMOptions", "-XX:ShenandoahNoProgressThreshold=12", "-XX:+ShowMessageBoxOnError",
                     "-XX:+UnlockExperimentalVMOptions", "-XX:ShenandoahNoProgressThreshold=12",
                     "-XX:+UseShenandoahGC", "-XX:ShenandoahGCMode=generational",
                     TestThreadFailure.class.getName(),
