f8f3b8d915044cbccb3e6be4e46e92f1353059f8
Kelvin Nilsen <kdnilsen@amazon.com>
2024-08-22 17:46:55 +0000

    Fix typo in comment

--- src/hotspot/share/gc/shenandoah/shenandoahUtils.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahUtils.hpp
@@ -242,7 +242,7 @@ public:
   }
 };
 
-// Proper units routines try to maintain at least three significant digits.  These variant assume
+// Proper units routines try to maintain at least three significant digits.  These variants assume
 // size is expressed in words, but the formatted output is to be expressed in bytes.
 inline const char* proper_unit_for_word_size(size_t words) {
   return proper_unit_for_byte_size(words * HeapWordSize);
