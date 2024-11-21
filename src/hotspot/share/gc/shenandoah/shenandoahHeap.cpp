9062113458bd1909409967abb743e11982f351ab
William Kemper <kemperw@amazon.com>
2024-11-21 13:35:23 -0800

    Remove vestigial debugging assertions

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -1967,9 +1967,6 @@ void ShenandoahHeap::set_gc_state(uint mask, bool value) {
   assert(ShenandoahSafepoint::is_at_shenandoah_safepoint(), "Must be at Shenandoah safepoint");
   _gc_state.set_cond(mask, value);
   _gc_state_changed = true;
-  // Check that if concurrent weak root is set then active_gen isn't null
-  assert(!is_concurrent_weak_root_in_progress() || active_generation() != nullptr, "Error");
-  shenandoah_assert_generations_reconciled();
 }
 
 void ShenandoahHeap::set_concurrent_young_mark_in_progress(bool in_progress) {
